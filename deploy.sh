83479f0bbef77580c17de8cf2e9f17f2cac53991
cindy <283175003@qq.com>
2022-02-23 21:14:39 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:cindyhan33/bamboo-ui-website.git &&
+git remote add origin git@gitee.com:lean-on-the-fence-to-listen/bamboo-ui.git &&
 git push -f -u origin master
